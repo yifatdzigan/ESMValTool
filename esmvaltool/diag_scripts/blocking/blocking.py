5f0d6eff37977287e9452d8c0e331f7b1bf06942
Javier Vegas-Regidor <javier.vegas@bsc.es>
2018-07-16 17:02:07 +0200

    First version of blocking 2D working

--- esmvaltool/diag_scripts/blocking/blocking.py
+++ esmvaltool/diag_scripts/blocking/blocking.py
@@ -1,6 +1,8 @@
 """Blocking diagnostic"""
 import os
 import logging
+import itertools
+import calendar
 
 import numpy as np
 import iris
@@ -46,14 +48,22 @@ class Blocking(object):
         self.datasets = esmvaltool.diag_scripts.shared.Datasets(self.cfg)
         self.variables = esmvaltool.diag_scripts.shared.Variables(self.cfg)
 
-        self.central_latitude = self.cfg.get('central_latitude', 60.0)
+        self.compute_1d = self.cfg.get('compute_1d', True)
+        self.compute_2d = self.cfg.get('compute_2d', True)
+
+
         self.span = self.cfg.get('span', 20.0)
-        self.offset = self.cfg.get('offset', 5.0)
         self.north_threshold = self.cfg.get('north_threshold', -10.0)
         self.south_threshold = self.cfg.get('south_threshold', 0.0)
-        self.months = self.cfg.get('months', list(range(1, 13)))
+        self.smoothing_window = self.cfg.get('smoothing_window', 1)
+        self.persistence = self.cfg.get('persistence', 1)
+
+        # 1D config
+        self.central_latitude = self.cfg.get('central_latitude', 60.0)
+        self.offset = self.cfg.get('offset', 5.0)
+
         self.max_color_scale = self.cfg.get('max_color_scale', 15)
-        self.smoothing_window = self.cfg.get('smoothing_window', 3)
+
         if self.smoothing_window % 2 == 0:
             raise ValueError('Smoothing should use an odd window '
                              'so the center of it is clear  ')
@@ -65,40 +75,101 @@ class Blocking(object):
         """Compute blocking diagnostic"""
         logger.info('Computing blocking')
         for filename in self.datasets:
-            zg500 = iris.load_cube(filename, 'geopotential_height')
 
-            if len(set(self.months)) != 12:
-                logger.info('Extracting months ...')
-                zg500 = zg500.extract(
-                    iris.Constraint(month_number=self.months))
-            iris.coord_categorisation.add_month(zg500, 'time')
 
-            results = iris.cube.CubeList([self._blocking_1d(zg500, month)
-                                          for month in self.months])
-            results.sort(key=lambda cube: cube.coord("month_number").points[0])
-            result = results.merge_cube()
-            if self.cfg[n.WRITE_NETCDF]:
-                new_filename = os.path.basename(filename).replace('zg',
-                                                                  'blocking')
-                netcdf_path = os.path.join(self.cfg[n.WORK_DIR],
-                                           new_filename)
-                iris.save(result, netcdf_path, zlib=True)
-            self._plot_result(filename, result)
+            result = self._blocking(filename)
+            self._blocking_1D(filename, result)
+            self._blocking_2D(filename, result)
 
-    def _plot_result(self, filename, result):
-        cmap = colors.LinearSegmentedColormap.from_list('mymap', (
-            (1, 1, 1), (0.7, 0.1, 0.09)), N=self.max_color_scale)
-        iris.quickplot.pcolormesh(result, coords=('longitude', 'month'),
-                                  cmap=cmap, vmin=0, vmax=self.max_color_scale)
-        plt.axis('tight')
-        plt.yticks(self.months)
-        plot_path = self._get_plot_name(filename)
-        plt.yticks(range(result.coord('month').shape[0]),
-                   result.coord('month').points)
-        axes = plt.gca()
-        axes.set_ylim((len(self.months) - 0.5, -0.5))
-        plt.savefig(plot_path)
-        plt.close()
+    def _blocking(self, filename):
+
+
+        zg500 = iris.load_cube(filename, 'geopotential_height')
+        iris.coord_categorisation.add_month(zg500, 'time')
+
+        lat = zg500.coord('latitude')
+        lat_max = np.max(lat.points)
+        lat_min = np.min(lat.points)
+
+        if self.compute_2d:
+            latitudes = lat.points
+        else:
+            latitudes = (self.central_latitude - self.offset,
+                         self.central_latitude,
+                         self.central_latitude + self.offset)
+
+        blocking = iris.cube.CubeList()
+        for lat_point in latitudes:
+            if lat_point + self.span > lat_max or \
+                lat_point -self.span < lat_min:
+                continue
+            logger.debug('Computing blocking for lat {0}'.format(lat_point))
+            blocking.append(self._compute_blocking(zg500, lat_point))
+
+        blocking_cube = blocking.merge_cube()
+        iris.coord_categorisation.add_year(blocking_cube, 'time')
+        return blocking_cube
+
+    def _blocking_1D(self, filename, blocking_index):
+        if not self.compute_1d:
+            return
+
+        lat = blocking_index.coord('latitude')
+
+        total_years = len(set(blocking_index.coord('year').points))
+
+        blocking = None
+        for displacement in [-self.offset, 0, self.offset]:
+            central = self.central_latitude + displacement
+            lat_value = lat.cell(lat.nearest_neighbour_index(central))
+            block = blocking_index.extract(iris.Constraint(latitude=lat_value)).data
+            if blocking_index is not None:
+                blocking = np.logical_or(blocking, block)
+            else:
+                blocking = block
+
+        blocking_cube = iris.cube.Cube(
+            blocking,
+            var_name="blocking",
+            attributes=None, )
+
+        blocking_cube.add_dim_coord(blocking_index.coord('time'), (0,))
+        blocking_cube.add_dim_coord(blocking_index.coord('longitude'), (1,))
+        blocking_cube.add_aux_coord(iris.coords.AuxCoord.from_coord(
+            blocking_index.coord('latitude').copy([self.central_latitude])))
+        iris.coord_categorisation.add_month_number(blocking_cube, 'time')
+
+        result = blocking_cube.aggregated_by('month_number',
+                                             iris.analysis.SUM)
+        result.remove_coord('time')
+        iris.util.promote_aux_coord_to_dim_coord(result, 'month_number')
+
+        result = self._smooth_over_longitude(result) / total_years
+
+        if not self.cfg[n.WRITE_NETCDF]:
+            new_filename = os.path.basename(filename).replace('zg',
+                                                              'blocking1D')
+            netcdf_path = os.path.join(self.cfg[n.WORK_DIR],
+                                       new_filename)
+            iris.save(result, target=netcdf_path, zlib=True)
+
+        if self.cfg[n.WRITE_PLOTS]:
+            iris.coord_categorisation.add_categorised_coord(
+                result, 'month', result.coord('month_number'),
+                lambda coord, x: calendar.month_abbr[x],
+                units='no_unit')
+            cmap = colors.LinearSegmentedColormap.from_list('mymap', (
+                (1, 1, 1), (0.7, 0.1, 0.09)), N=self.max_color_scale)
+            iris.quickplot.pcolormesh(result, coords=('longitude', 'month'),
+                                      cmap=cmap, vmin=0, vmax=self.max_color_scale)
+            plt.axis('tight')
+            plt.yticks(self.months)
+            plot_path = self._get_plot_name(filename)
+            plt.yticks(range(result.coord('month').shape[0]),
+                       result.coord('month').points)
+            axes = plt.gca()
+            axes.set_ylim((len(self.months) - 0.5, -0.5))
+            plt.savefig(plot_path, bbox_inches='tight', pad_inches=0.2)
 
     def _get_plot_name(self, filename):
         dataset = self.datasets.get_info(n.DATASET, filename)
@@ -108,7 +179,7 @@ class Blocking(object):
         end = self.datasets.get_info(n.END_YEAR, filename)
         out_type = self.cfg[n.OUTPUT_FILE_TYPE]
 
-        plot_filename = 'blocking_{project}_{dataset}_' \
+        plot_filename = 'blocking1D_{project}_{dataset}_' \
                         '{ensemble}_{start}-{end}' \
                         '.{out_type}'.format(dataset=dataset,
                                              project=project,
@@ -121,64 +192,37 @@ class Blocking(object):
                                  plot_filename)
         return plot_path
 
-    def _blocking_1d(self, zg500, month):
-        logger.info('Computing month %s...', month)
-        zg500 = zg500.extract(iris.Constraint(month_number=month))
-        total_years = len(set(zg500.coord('year').points))
-
-        blocking_index = None
-        for displacement in [-self.offset, 0, self.offset]:
-            central = self.central_latitude + displacement
-            block = self._compute_blocking(zg500, central)
-            if blocking_index is not None:
-                blocking_index = np.logical_or(blocking_index, block)
-            else:
-                blocking_index = block
-
-        blocking_frequency = np.count_nonzero(blocking_index, 0) / total_years
-        blocking_frequency = self._smooth_over_longitude(blocking_frequency)
-        blocking_cube = iris.cube.Cube(
-            blocking_frequency,
-            var_name="block1d",
-            units="Days per month",
-            long_name="Blocking pattern",
-            attributes=None,)
-        blocking_cube.add_dim_coord(zg500.coord('longitude'), 0)
-        month_coord = zg500.coord('month_number').copy(month)
-        blocking_cube.add_aux_coord(month_coord)
-        month_coord = zg500.coord('month').copy(zg500.coord('month').points[0])
-        blocking_cube.add_aux_coord(month_coord)
-
-        return blocking_cube
-
     def _smooth_over_longitude(self, cube):
         if self.smoothing_window == 1:
             return cube
         logger.debug('Smoothing...')
-        smooth = np.zeros_like(cube)
-        size = cube.shape[0]
-        window = self.smoothing_window // 2
-        for x in range(0, window):
-            win = np.concatenate((cube[0: x + window],
-                                  cube[- window + x:]))
-            smooth[x] = np.mean(win)
+        for month_slice in cube.slices_over('month_number'):
+            smooth = np.zeros_like(month_slice.data)
+            size = month_slice.shape[0]
+            window = self.smoothing_window // 2
+            for x in range(0, window):
+                win = np.concatenate((month_slice.data[0: x + window],
+                                      month_slice.data[- window + x:]))
+                smooth[x] = np.mean(win)
 
-        for x in range(window, size - window):
-            smooth[x] = np.mean(cube[x - window: x + window])
+            for x in range(window, size - window):
+                smooth[x] = np.mean(month_slice.data[x - window: x + window])
 
-        for x in range(size - window, size):
-            win = np.concatenate((cube[x - window:],
-                                  cube[0: x + window - size + 1]))
-            smooth[x] = np.mean(win)
+            for x in range(size - window, size):
+                win = np.concatenate((month_slice.data[x - window:],
+                                      month_slice.data[0: x + window - size + 1]))
+                smooth[x] = np.mean(win)
+            month_slice.data[...] = smooth[...]
         logger.debug('Smoothing finished!')
-        return smooth
+        return cube
 
     def _compute_blocking(self, zg500, central_latitude):
+
         latitude = zg500.coord('latitude')
+
 
         def _get_lat_cell(coord, latitude):
             return coord.cell(coord.nearest_neighbour_index(latitude))
-
         central_lat = _get_lat_cell(latitude, central_latitude)
         low_lat = _get_lat_cell(latitude, central_latitude - self.span)
         high_lat = _get_lat_cell(latitude, central_latitude + self.span)
@@ -187,13 +231,100 @@ class Blocking(object):
         zg_central = zg500.extract(iris.Constraint(latitude=central_lat))
         zg_high = zg500.extract(iris.Constraint(latitude=high_lat))
 
+
         north_gradient = (zg_high - zg_central) / \
                          (high_lat.point - central_lat.point)
         south_gradient = (zg_central - zg_low) / \
                          (central_lat.point - low_lat.point)
 
-        return np.logical_and(south_gradient.data > self.south_threshold,
-                              north_gradient.data < self.north_threshold)
+        blocking_index = np.logical_and(
+            south_gradient.data > self.south_threshold,
+            north_gradient.data < self.north_threshold
+        )
+
+
+        blocking_cube = iris.cube.Cube(
+            blocking_index,
+            var_name="blocking",
+            units="Days per month",
+            long_name="Blocking pattern",
+            attributes=None, )
+        blocking_cube.add_aux_coord(zg500.coord('month_number'), (0,))
+        blocking_cube.add_aux_coord(iris.coords.AuxCoord.from_coord(
+            zg500.coord('latitude').copy([central_latitude])))
+        blocking_cube.add_dim_coord(zg500.coord('time'), (0,))
+        blocking_cube.add_dim_coord(zg500.coord('longitude'), (1,))
+
+        if self.persistence > 1:
+            for lon_slice in blocking_cube.slices_over('longitude'):
+                grouped = ((k, sum(1 for _ in g))
+                           for k, g in itertools.groupby(lon_slice.data))
+                index = 0
+                for value, length in grouped:
+                    if value and length < self.persistence:
+                        lon_slice.data[index: index + length] = False
+                    index += length
+        return blocking_cube
+
+    def _blocking_2D(self, filename, blocking_index):
+        if not self.compute_2d:
+            return
+        total_years = len(set(blocking_index.coord('year').points))
+        blocking_index = blocking_index.aggregated_by(
+            'month_number', iris.analysis.SUM) / total_years
+
+        blocking_index.long_name = 'Blocking index'
+        blocking_index.units = 'Days per month'
+
+
+        if self.cfg[n.WRITE_NETCDF]:
+            new_filename = os.path.basename(filename).replace('zg',
+                                                              'blocking')
+            netcdf_path = os.path.join(self.cfg[n.WORK_DIR],
+                                       new_filename)
+            iris.save(blocking_index, netcdf_path, zlib=True)
+
+        if self.cfg[n.WRITE_PLOTS]:
+            for month_slice in blocking_index.slices_over('month_number'):
+                cmap = colors.LinearSegmentedColormap.from_list('mymap', (
+                    (1, 1, 1), (0.7, 0.1, 0.09)), N=self.max_color_scale)
+                iris.quickplot.pcolormesh(
+                    month_slice,
+                    coords=('longitude', 'latitude'),
+                    cmap=cmap, vmin=0, vmax=self.max_color_scale
+                )
+                plot_path = self._get_plot_name_2D(
+                    filename,
+                    month_slice.coord('month_number').points[0]
+                )
+                plt.gca().set_aspect(1.5, 'box')
+                plt.gca().coastlines()
+                plt.savefig(plot_path, bbox_inches='tight', pad_inches=0.2)
+                plt.close()
+
+    def _get_plot_name_2D(self, filename, month):
+        dataset = self.datasets.get_info(n.DATASET, filename)
+        project = self.datasets.get_info(n.PROJECT, filename)
+        ensemble = self.datasets.get_info(n.ENSEMBLE, filename)
+        start = self.datasets.get_info(n.START_YEAR, filename)
+        end = self.datasets.get_info(n.END_YEAR, filename)
+        out_type = self.cfg[n.OUTPUT_FILE_TYPE]
+        month=calendar.month_abbr[month]
+
+        plot_filename = 'blocking2D_{month}_{project}_{dataset}_' \
+                        '{ensemble}_{start}-{end}' \
+                        '.{out_type}'.format(dataset=dataset,
+                                             project=project,
+                                             ensemble=ensemble,
+                                             start=start,
+                                             end=end,
+                                             out_type=out_type,
+                                             month=month)
+
+        plot_path = os.path.join(self.cfg[n.PLOT_DIR],
+                                 plot_filename)
+        return plot_path
+
 
 
 if __name__ == '__main__':
